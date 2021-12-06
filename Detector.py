fdb2b59b2b429d960f8309367a1557debc509938
hitesh314 <84220808+hitesh314@users.noreply.github.com>
2021-12-07 00:00:49 +0530

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -1,55 +1,89 @@
-import numpy as np
-import pyautogui
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 13:57:54 2021
+
+@author: Hitesh
+"""
 import cv2
-prev_pos="neutral"
+import numpy as np
+import matplotlib.pyplot as plt
+red_light=False
+green_light=False
+yellow_light=False
+img= cv2.imread('C:/all desktop folders/EMR/pics/traffic/red3.jpeg',1)
+rectangle = cv2.imread('C:/all desktop folders/EMR/pics/traffic/red3.jpeg',1)
+rectangle   = cv2.inRange(rectangle, (100,0,0) , (225,80,80))
+#rectangle = cv2.imread('C:/all desktop folders/EMR/pics/traffic/red2.jpeg', cv2.IMREAD_GRAYSCALE)
+def cornerPoints(rectangle,image):
+    x_max=0
+    x_min=1000
+    y_min=1000
+    y_max=0
+    rectangle = np.where(rectangle > np.mean(rectangle), 255, 0).astype(np.uint8)    
+    dst_rectangle = cv2.cornerHarris(rectangle, 2, 3, 0.04)
+    dst_rectangle = cv2.dilate(dst_rectangle, None)
+    mask = np.where(dst_rectangle > 0.01*np.max(dst_rectangle), 255, 0).astype(np.uint8)
+    points = np.nonzero(mask)
+    j=len(points[0])
+    f=len(points[1])
+    s=0
+    k=0
+    while k<j:
+        x=points[0][k]
+        y=points[1][k]
+        if image[y,x,0]>128 and image[y,x,0]<= 255 and image[y,x,1]<127 and image[y,x,2]<127 :
+            if x>x_max:
+                x_max=x
+            if x<x_min:
+                x_min=x
+        k=k+1
+    y_min=x_min
+    y_max=x_max
+    t=(x_min,y_min,x_max,x_max)
+    return t
 
-vid=cv2.VideoCapture(0)
-while(1):
-    _,frame=vid.read()
-    frame=cv2.flip(frame,1)
-    frame=frame[:300,0:400]
-    frame=cv2.GaussianBlur(frame,(5,5),0)
-    lower_skin=np.array([13,16,28])
-    upper_skin=np.array([87,93,125])
-    mask=cv2.inRange(frame,lower_skin,upper_skin)
-    _,thresh=cv2.threshold(mask, 127, 255,cv2.THRESH_BINARY)
-    contours,hierarchy=cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    img = cv2.drawContours(frame , contours , -1 , (0,0,255) , 2)
-    if len(contours)==0:
-        continue
-    max_contour=max(contours,key=cv2.contourArea)
-    epsilon=0.01*cv2.arcLength(max_contour,True)
-    approx=cv2.approxPolyDP(max_contour,epsilon,True)
-    M=cv2.moments(approx)
-    try:
-        x=int(M['m10']/M['m00'])
-        y=int(M['m01']/M['m00'])
-    except ZeroDivisionError:
-        continue
-    frame=cv2.circle(frame,(x,y),10,(0,255,0),3)
-    frame=cv2.line(frame,(125,0),(125,400),(0,0,255),2)
-    frame=cv2.line(frame,(225,0),(225,400),(0,0,255),2)
-    frame=cv2.line(frame,(0,200),(400,200),(0,0,255),2)
-    frame=cv2.line(frame,(0,250),(400,250),(0,0,255),2)
-    cv2.imshow('Contoors',frame)
+print(cornerPoints(rectangle,img))
+X_min=cornerPoints(rectangle,img)[0]
+Y_min=cornerPoints(rectangle,img)[1]
+X_max=cornerPoints(rectangle,img)[2]
+Y_max=cornerPoints(rectangle,img)[3]
+print(X_min,Y_min,X_max,Y_max)
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+circles=cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=26, minRadius=0, maxRadius=0)
+#print(circles)
+circlesnumber=len(circles[0])
+acc=0
+while acc<circlesnumber:
+    print("circle",acc+1)
+    X=circles[0][acc][0]
+    Y=circles[0][acc][1]
+    X=int(X)
+    Y=int(Y)
+    if X>X_min and X<X_max and Y>Y_min and Y<Y_max:
+        print('pass1')
+        print(X,Y)
+        print(img[Y,X])
+        if img[Y,X,0]<20 and img[Y,X,1]>200 and img[Y,X,2]<20:
+            print('pass2')
+            green_light=True
+        elif(img[Y,X,0]<127 and img[Y,X,1]>128 and img[Y,X,1]<=255 and img[Y,X,2]>128 and img[Y,X,2]<=255): 
+            yellow_light=True
+            print('pass3')
+        elif(img[Y,X,0]<20 and img[Y,X,1]<20 and img[Y,X,2]>200):
+            red_light=True
+            print("pass4")
+        acc=acc+1
+#cv2.imshow('image',img)
+#cv2.imshow('rectangle',rectangle)
 
-    if x<125:
-        curr_pos="left"
-    elif x>225:
-        curr_pos="right"
-    elif y<200 and x>125 and x>225:
-        curr_pos="up"
-    elif y>255 and x>125 and x<225:
-        curr_pos="down"
-    else:
-        curr_pos="neutral"
-    if curr_pos!=prev_pos:
-        if curr_pos !="neutral":
-            pyautogui.press(curr_pos)
-        prev_pos=curr_pos
-
-
-    if cv2.waitKey(1)==ord('q'):
-         break
-vid.release()
+print(circlesnumber)
+if red_light==True and yellow_light==False and green_light==False:
+    print("The light is red")
+elif yellow_light==True and red_light==False and green_light==False:
+    print("The light is yellow")
+elif green_light==True and red_light==False and yellow_light==False:
+    print("The light is green")
+else:
+    print("Try again")
+cv2.waitKey(0)
 cv2.destroyAllWindows()
